23cbabf53da8338fe98efaad5ed97518f7905ce4
ekim6094 <jongmanator@gmail.com>
2020-07-12 21:53:59 -0400

    php mysqli connection reuse

---
+++ combined_code/php/conn.php
@@ -0,0 +1,18 @@
+<?php
+// php functions for msqli connection thing
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "cmsc433_proj3";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+?>
